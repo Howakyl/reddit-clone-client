935b9b83e3d627b0b61cad61f803b94cd780e7a2
Howakyl96 <howakyl@gmail.com>
2021-03-06 13:37:57 -0800

    register component connected to graphql backend

--- src/pages/register.tsx
+++ src/pages/register.tsx
@@ -3,18 +3,36 @@ import {Formik, Form} from 'formik';
 import { Box, Button, FormControl, FormErrorMessage, FormLabel, Input } from '@chakra-ui/react';
 import { Wrapper } from '../components/Wrapper';
 import { InputField } from '../components/InputField';
+import { useMutation } from 'urql';
 
 interface registerProps {
 
 }
 
+const REGISTER_MUT = `
+mutation Register($username: String!, $password:String!){
+  register(options: {username:$username, password: $password}) {
+    errors {
+      field
+      message
+    }
+    
+    user {
+      id
+      username
+    }
+  }
+}
+`
+
 export const Register: React.FC<registerProps> = ({}) => {
+  const [{},register] = useMutation(REGISTER_MUT)
   return (
     <Wrapper variant="small">
       <Formik 
         initialValues={{username: "", password: "" }}
         onSubmit={(values) => {
-          console.log(values)
+          register(values);
         }}
       >
         {({isSubmitting}) => (
