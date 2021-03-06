f6145609b78172c35db5e6552420f29142be1949
Howakyl96 <howakyl@gmail.com>
2021-03-06 14:12:05 -0800

    imported graphql codegen, graphql folder for mutations and queries

--- src/pages/register.tsx
+++ src/pages/register.tsx
@@ -26,13 +26,13 @@ mutation Register($username: String!, $password:String!){
 `
 
 export const Register: React.FC<registerProps> = ({}) => {
-  const [{},register] = useMutation(REGISTER_MUT)
+  const [,register] = useMutation(REGISTER_MUT)
   return (
     <Wrapper variant="small">
       <Formik 
         initialValues={{username: "", password: "" }}
-        onSubmit={(values) => {
-          return register(values);
+        onSubmit = {async (values) => {
+          const response = await register(values);
         }}
       >
         {({isSubmitting}) => (
