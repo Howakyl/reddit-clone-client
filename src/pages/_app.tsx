1e658236bccb7e3746ec26f07047e2afb20d5247
Howakyl96 <howakyl@gmail.com>
2021-03-15 16:49:29 -0700

    helper function in app.tsx to update navbar if login state changes, with typing

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,15 +1,42 @@
 import { ChakraProvider, ColorModeProvider } from '@chakra-ui/react'
 import React from 'react'
 import { Provider, createClient, dedupExchange, fetchExchange } from 'urql'
-import { cacheExchange } from '@urql/exchange-graphcache';
+import { cacheExchange, Cache, QueryInput } from '@urql/exchange-graphcache';
 import theme from '../theme';
+import { LoginMutation, MeDocument, MeQuery } from '../generated/graphql';
+
+function betterUpdateQuery<Result, Query> (
+  cache: Cache,
+  qi: QueryInput,
+  result: any,
+  fn: (r: Result, q: Query) => Query
+) {
+  return cache.updateQuery(qi, data => fn(result, data as any) as any);
+}
 
 const client = createClient({ 
   url: 'http://localhost:4000/graphql',
   fetchOptions: {
     credentials: "include",
   },
-  exchanges: [dedupExchange, cacheExchange({}), fetchExchange]
+  exchanges: [dedupExchange, cacheExchange({
+    updates: {
+      Mutation: {
+        login: (result, args, cache, info) => {
+            betterUpdateQuery<LoginMutation, MeQuery>(cache, {query: MeDocument}, result,
+              (result, query) => {
+                if (result.login.errors) {
+                  return query
+                } else {
+                  return {
+                    me: result.login.user
+                  }
+                }
+              })
+        },
+      },
+    },
+  }), fetchExchange]
 });
 
 function MyApp({ Component, pageProps }: any) {
